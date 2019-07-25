2d163e8015401a739cf9e9fee3b33aa383132711
Shaikh Naushad <shanaush@publicisgroupe.net>
2019-07-25 17:21:41 +0530

    Thursday

---
+++ UI/Practice/script.js
@@ -0,0 +1,27 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <h1>JavaScript</h1>
+        <hr>
+            <script>
+               
+            for(int i=0;i<=10;i++)
+                          {
+                    document.write("Hello <br>");
+                           }
+                </script>
+      </body>
+  </html>
+
+
+
+
+
+
+
+
+
+
+
+
