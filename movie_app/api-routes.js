c07ccd1fc22cacc921e04b12c05ea31a55955712
Shaikh Naushad <shanaush@publicisgroupe.net>
2019-07-30 17:39:44 +0530

    30Tuesday

---
+++ movie_app/api-routes.js
@@ -0,0 +1,17 @@
+let router = require('express').Router();
+let MoviesController = require('./moviesController')
+
+router.get('/',(req,res)=>{
+    res.json({
+        status:'API is working',
+        message:'MoviesApp REST API'
+    })
+});
+router.route('/movies')
+    .get(MoviesController.listAll)
+    .post(MoviesController.create);
+router.route('/movies/:id')
+    .get(MoviesController.findByID)
+    .delete(MoviesController.delete)
+    
+module.exports= router;
